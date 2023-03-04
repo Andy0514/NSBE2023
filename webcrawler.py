84df9d938488f3b271a3009a8978c60a78c1d6f2
Andy Wang <wanghaolinsh@icloud.com>
2023-03-04 16:09:18 -0500

    Add starter webcrawler and speech recognition code

--- webcrawler.py
+++ webcrawler.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 count = 0
 videos = []
-for i in range(1, 10950):
+for i in range(1, 10):
     url = "https://www.handspeak.com/word/" + str(i) + "/"
 
     # Get URL Content
@@ -17,13 +17,15 @@ for i in range(1, 10950):
     for video_tag in video_tags:
         video_url = video_tag['src']
         if video_url != None:
-            videos.append(video_url)
+            videos.append("https://www.handspeak.com" + video_url)
+            break
 
     count += 1
     if (count * 100) / 10950 in {10, 20, 30, 40, 50, 60, 70, 80, 90}:
         print(str((count * 100) / 10950) + "% finished")
 
 print("Found " + str(len(videos)) + " videos")
+print(videos)
 
 
 
