37a42485b09b4fb74411f3ab67acd351b017c2ff
Jessica Tang <jessicatang2019@gmail.com>
2023-03-05 09:29:15 -0500

    added emoji sentiments

--- interface.py
+++ interface.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk
 import speech_recognition
 import text_processing_pipeline
 from tkvideo import tkvideo
+import emoji
 
 # set up
 root = Tk() 
@@ -13,23 +14,20 @@ root.configure(bg=bg_col)
 root.title("Speech to ASL")
                     
 Label(root, text='\narticulator', font =('Times New Roman', '30'), bg = bg_col, fg = 'white').pack(padx=20, anchor='w') # title
-
 def emoji_display(sentiment):
     # ['anger', 'fear', 'sadness', 'joy', 'love', 'surprise']
-    # if sentiment == 'anger':
-    #    anger_img = Image.open("images/anger.png")
-    #    anger_img = anger_img.resize((50, 50))
-    #    anger_img.place(x=160, y=60)
-    # elif sentiment == 'fear':
-    # joy_img = Image.open("images/microphone.png")
-    # joy_img = joy_img.resize((50, 50))
-    # joy_img = ImageTk.PhotoImage(joy_img) 
-    # Label(root, image = joy_img).pack()
-    image = Image.open('images/switch.png') # Load microphone image
-    img = image.resize((150, 150))
-    switch = ImageTk.PhotoImage(img) # convert img
-    img_label= Label(image = switch)
-    img_label.place(x=1000, y=100) 
+   if (sentiment == 'anger'):
+      Label(root, text=emoji.emojize(":enraged_face:"), font =('Times New Roman', '30'), bg = bg_col, fg = 'white').place(x = 450, y = 600)
+   if (sentiment == 'fear'):
+      Label(root, text=emoji.emojize(":fearful_face:"), font =('Times New Roman', '30'), bg = bg_col, fg = 'white').place(x = 450, y = 600)
+   if (sentiment == 'sadness'):
+      Label(root, text=emoji.emojize(":sad_but_relieved_face:"), font =('Times New Roman', '30'), bg = bg_col, fg = 'white').place(x = 450, y = 600)
+   if (sentiment == 'joy'):
+      Label(root, text=emoji.emojize(":grinning_face_with_big_eyes:"), font =('Times New Roman', '30'), bg = bg_col, fg = 'white').place(x = 450, y = 600)
+   if (sentiment == 'love'):
+      Label(root, text=emoji.emojize(":smiling_face_with_hearts:"), font =('Times New Roman', '30'), bg = bg_col, fg = 'white').place(x = 450, y = 600)
+   if (sentiment == 'surprise'):
+      Label(root, text=emoji.emojize(":anguished_face:"), font =('Times New Roman', '30'), bg = bg_col, fg = 'white').place(x = 450, y = 600)
 
 def helloCallBack():
    recognized_text = speech_recognition.recognize_from_microphone()
@@ -40,10 +38,10 @@ def helloCallBack():
    emoji_display(sentiment)
    my_label = Label(root)
    my_label.pack(anchor='w', padx=50)
-   player = tkvideo("interested.mp4", my_label, loop = 1, size = (850,450))
+   player = tkvideo("videos/interested.mp4", my_label, loop = 1, size = (850,450))
    player.play()
 
-image = Image.open('images/microphone.png') # Load microphone image
+image = Image.open("images/microphone.png") # Load microphone image
 img = image.resize((150, 150))
 mic = ImageTk.PhotoImage(img) # convert img
 record_btn = mic
